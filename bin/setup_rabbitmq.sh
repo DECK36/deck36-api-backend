fc8e9eafa511a86ef2e36afe9997a89eae25e585
Vagrant User <vagrant@plan9.local>
2014-10-21 09:19:06 +0000

    fixed rabbitmq setup script

--- bin/setup_rabbitmq.sh
+++ bin/setup_rabbitmq.sh
@@ -5,5 +5,4 @@ wget http://127.0.0.1:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 
 ./rabbitmqadmin --port=15672 --host=127.0.0.1 declare exchange name=plan9 type=topic auto_delete=false durable=true
-./rabbitmqadmin --port=15672 --host=127.0.0.1 declare exchange name=plan9-backchannel type=fanout auto_delete=false
-durable=true
+./rabbitmqadmin --port=15672 --host=127.0.0.1 declare exchange name=plan9-backchannel type=topic auto_delete=false durable=true
